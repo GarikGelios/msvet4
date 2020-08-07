71bf99f4d4ab02fbc10193e3eb2514844917048a
GarikGelios <garikgelios@gmail.com>
2020-08-07 12:51:02 +0300

    edit email sittings

--- nodemailer.js
+++ nodemailer.js
@@ -5,9 +5,9 @@ const nodemailer = require('nodemailer');
 
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
-    host: 'in-dev.work', // Сервер исходящей почты (SMTP) из настроек Gmail
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    host: 'mailbe05.hoster.by', // Сервер исходящей почты (SMTP) из настроек Gmail
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL,   // тянется из .env
         pass: process.env.PASSWORD // тянется из .env
