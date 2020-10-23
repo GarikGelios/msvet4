b533e0bcfdf9c161599b39a39dd3c847f0ce7c18
GarikGelios <garikgelios@gmail.com>
2020-10-23 23:11:48 +0300

    add npm  package vue-gtm

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueGtm from 'vue-gtm'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
@@ -100,8 +101,7 @@ const routes = [
       metaTags: [
         {
           name: 'description',
-          content:
-            'Напишите нам'
+          content: 'Напишите нам'
         },
         {
           name: 'keywords',
@@ -113,8 +113,7 @@ const routes = [
         },
         {
           property: 'og:description',
-          content:
-            'Напишите нам'
+          content: 'Напишите нам'
         },
         {
           property: 'og:type',
@@ -133,6 +132,17 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+Vue.use(VueGtm, {
+  id: 'GTM-KBQNXSQ', // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']
+  defer: false, // defaults to false. Script can be set to `defer` to increase page-load-time at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible)
+  enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+  debug: true, // Whether or not display console logs debugs (optional)
+  loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
+  vueRouter: router, // Pass the router instance to automatically sync with router (optional)
+  ignoredViews: ['homepage'], // Don't trigger events for specified router names (case insensitive) (optional)
+  trackOnNextTick: false // Whether or not call trackView in Vue.nextTick
 })
 
 // функция которая встроит meta-теги в нужные места сайта
